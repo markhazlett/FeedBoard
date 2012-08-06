ab8dac4c0e7b6c925541c6e8b519409d01b76a71
Mark Hazlett <mark@icog-studios.com>
2012-08-06 10:25:57 -0600

    Adding styles and new feeds + new README

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,4 +1,11 @@
+require 'feedzirra'
+
 class IndexController < ApplicationController
   def index
+    @feed = Feedzirra::Feed.fetch_and_parse("http://feeds.macrumors.com/MacRumors-All")  
+
+    @feed2 = Feedzirra::Feed.fetch_and_parse("http://feeds.feedburner.com/theappleblog")
+
+    @feed3 = Feedzirra::Feed.fetch_and_parse("http://feeds.feedburner.com/9To5Mac-MacAllDay")    
   end
 end
